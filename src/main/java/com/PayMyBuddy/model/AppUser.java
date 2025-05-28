d3a2c51e3fe665c074f0c9d0df72347cd994dd39
Nabil Ben khemis <beardydjib@gmail.com>
2025-05-28 10:55:16 +0200

    Initial commit

---
+++ src/main/java/com/PayMyBuddy/model/AppUser.java
@@ -0,0 +1,20 @@
+package com.PayMyBuddy.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
